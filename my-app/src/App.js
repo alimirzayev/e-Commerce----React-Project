2bb0bd048bfff0706c59da0145583e58d047b4e4
alimirzayev <alimirzayev0414@gmail.com>
2021-11-19 14:04:43 +0400

    start context

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import React, { useState } from 'react'
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { CartProvider, useCart } from "react-use-cart";
+
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer';
 import Home from './Page/Home';
 import Checkout from './Page/Checkout';
 import Detail from './Page/Detail';
 import Categorypage from './Page/Categorypage';
-import Rating from '@mui/material/Rating';
-import Typography from '@mui/material/Typography';
 
 function App() {
-  const [value, setValue] = useState(4);
+
   return (
     <div className="App">
 
