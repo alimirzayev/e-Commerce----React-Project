c53b864c54f223145905d80a70c8ddb285e65911
alimirzayev <alimirzayev0414@gmail.com>
2021-12-06 15:33:51 +0400

    start hamburger icon

--- my-app/src/components/Responsive.jsx
+++ my-app/src/components/Responsive.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Main from '../components/Main/Main'
 import ReadBlog from '../components/ReadBlog/ReadBlog'
+import Testimonial from '../components/Testimonial/Testimonial'
 
 function Responsive() {
     return (
         <div>
             <Main />
+            <Testimonial />
             <ReadBlog />
         </div>
     )
