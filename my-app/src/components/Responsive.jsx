6cec28a96564ba732b66aff2f2deb7fb3cb01a23
alimirzayev <alimirzayev0414@gmail.com>
2021-12-06 12:58:42 +0400

    blogpage responsive added

--- my-app/src/components/Responsive.jsx
+++ my-app/src/components/Responsive.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Main from '../components/Main/Main'
+import ReadBlog from '../components/ReadBlog/ReadBlog'
 
 function Responsive() {
     return (
         <div>
             <Main />
+            <ReadBlog />
         </div>
     )
 }
